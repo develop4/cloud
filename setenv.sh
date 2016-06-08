171df19e2791570f243291d0977270c43ae154ea
timpwi <william.timpany@develop4.co.uk>
2016-06-08 18:20:24 +0100

    initial checkin

---
+++ setenv.sh
@@ -0,0 +1,6 @@
+
+export CONFIG_SERVICE_PASSWORD=mypassword 
+export MONGODB_PASSWORD=mypassword 
+export ACCOUNT_SERVICE_PASSWORD=mypassword 
+export STATISTICS_SERVICE_PASSWORD=mypassword 
+export NOTIFICATION_SERVICE_PASSWORD=mypassword 
